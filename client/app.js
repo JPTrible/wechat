9958e5a08ef7954218554e6752afb6151a76834f
wuych <e14_100005868412_wuych@git.cloud.tencent.com>
2018-07-19 10:24:36 +0800

    '添加新页面'

--- client/app.js
+++ client/app.js
@@ -5,5 +5,19 @@ var config = require('./config')
 App({
     onLaunch: function () {
         qcloud.setLoginUrl(config.service.loginUrl)
-    }
+    },
+
+    onShow: function(options){
+
+    },
+
+    onHide: function(options){
+
+    },
+
+    onError: function(msg){
+      console.log(msg)
+    },
+
+    globalData: "I am global data"
 })
